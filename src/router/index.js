f8b6710aca4bf58acd8014be5e56b1597a4813f5
ilyyzy <1361546044@qq.com>
2021-10-14 20:44:21 +0800

    登录和首页头部

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,36 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import Home from '../views/Home.vue'
+
+// 导入页面组件
+import Login from '@/views/Login'
+import Main from '@/views/Main'
+import Home from '@/views/Home'
+import User from '@/views/User'
 
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    path: '/login',
+    component: Login,
+    name: 'login'
+  },
+  {
+    path: '/',
+    component: Main,
+    children: [
+      {
+        path: '/',
+        component: Home,
+        name: 'home'
+      },
+      {
+        path: '/user',
+        component: User,
+        name: 'user'
+      }
+    ]
+  }
 ]
 
 const router = new VueRouter({
